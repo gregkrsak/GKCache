cea9a76fa575aa9a6b307bd45cf15e2d305c0903
Greg Krsak <greg.krsak@gmail.com>
2013-08-20 14:51:43 -0700

    Created Xcode 5 project

---
+++ GKCache/GKCacheTests/GKCacheTests.m
@@ -0,0 +1,34 @@
+//
+//  GKCacheTests.m
+//  GKCacheTests
+//
+//  Created by Greg M. Krsak on 8/20/13.
+//  Copyright (c) 2013 Greg M. Krsak. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GKCacheTests : XCTestCase
+
+@end
+
+@implementation GKCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
